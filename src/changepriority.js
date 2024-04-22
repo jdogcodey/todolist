c8c7bd5ed3b41632440df9cccb51ec0e1e2c8d6e
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-04-22 16:01:14 +0100

    Add function to change priority

---
+++ src/changepriority.js
@@ -0,0 +1,7 @@
+// Function to change the priority of a todo
+
+function changePriority(todo, newpriority) {
+  todo["priority"] = newpriority;
+}
+
+export { changePriority };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { newToDo } from "./newtodo.js";
 import { newList } from "./newlist.js";
 import { moveList } from "./movelist.js";
 import { completeToDo } from "./completetodo.js";
+import { changePriority } from "./changepriority.js";
 
 const globalToDoList = [];
 newToDo(
@@ -13,7 +14,20 @@ newToDo(
   "Description goes here"
 );
 
-document.querySelector("button").addEventListener("click", () => {
-  completeToDo(globalToDoList[0]);
+document.querySelector("#low").addEventListener("click", () => {
+  changePriority(globalToDoList[0], "Low");
+  console.log("Change priority to low");
+  console.log(globalToDoList);
+});
+
+document.querySelector("#med").addEventListener("click", () => {
+  changePriority(globalToDoList[0], "Med");
+  console.log("Change priority to med");
+  console.log(globalToDoList);
+});
+
+document.querySelector("#high").addEventListener("click", () => {
+  changePriority(globalToDoList[0], "High");
+  console.log("Change priority to high");
   console.log(globalToDoList);
 });
